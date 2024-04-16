8dc080c6828a4d5a3b6f44dcc9845e1de61b73b1
Cao Viet Anh <caovietanhbk@gmail.com>
2024-04-16 17:20:11 +0700

    Finished Layout for landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
+  console.log('render children');
   return (
     <div>
       <h1 className="text-green-500">Hello Lingo!</h1>
