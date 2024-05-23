9e8d268adf716e78e7517144afa710b08982b575
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-23 23:48:29 +0700

    Add lesson button

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,14 +1,19 @@
 import { StickyWrapper } from '@/components/sticky-wrapper';
 import { FeedWrapper } from '@/components/feed-wrapper';
 import { Header } from './_components/header';
+import { Unit } from './_components/unit';
 import { UserProgress } from '@/components/user-progress';
-import { getUserProgress } from '@/db/queries';
+import { getUnits, getUserProgress } from '@/db/queries';
 import { redirect } from 'next/navigation';
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect('/courses');
@@ -18,14 +23,27 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: 'English', imageSrc: '/uk.svg' }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="English" />
+        <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={undefined}
+              activeLessonPercentage={0}
+            />
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,9 +9,12 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
+        locked:
+          'bg-neutral-200 text-primary-foreground hover:bg-neutral-200/90 border-neutral-400 border-b-4 active:border-b-0',
         default:
           'bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500',
-        primary: 'bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0',
+        primary:
+          'bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0',
         primaryOutline: 'bg-white text-sky-500 hover:bg-slate-100',
         secondary:
           'bg-green-500 text-secondary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0',
@@ -22,9 +25,12 @@ const buttonVariants = cva(
         super:
           'bg-indigo-500 text-secondary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0',
         superOutline: 'bg-white text-indigo-500 hover:bg-slate-100',
-        ghost: 'bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100',
-        sidebar: 'bg-transparent text-slate-500 border-2 border-transparent hover:bg-slate-100 transition-none',
-        sidebarOutline: 'bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none',
+        ghost:
+          'bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100',
+        sidebar:
+          'bg-transparent text-slate-500 border-2 border-transparent hover:bg-slate-100 transition-none',
+        sidebarOutline:
+          'bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none',
       },
       size: {
         default: 'h-11 px-4 py-2',
@@ -50,7 +56,13 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
   },
 );
 Button.displayName = 'Button';
