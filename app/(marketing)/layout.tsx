7a928ea8cf8bc6f3f672c1810f34bccbe0e132d5
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-23 10:50:29 +0700

    Add main layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
-import { Header } from './header';
-import { Footer } from './footer';
+import { Header } from './_components/header';
+import { Footer } from './_components/footer';
 
 type Props = {
   children: React.ReactNode;
