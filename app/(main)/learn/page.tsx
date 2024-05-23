f52b99ae76c24dc2466d5a95b49fa23fbdb4e2a5
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-23 11:54:49 +0700

    Add feed wrapper and sticky header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,24 @@
+import { StickyWrapper } from '@/components/sticky-wrapper';
+import { FeedWrapper } from '@/components/feed-wrapper';
+import { Header } from './_components/header';
+import { UserProgress } from '@/components/user-progress';
+
 const LearnPage = () => {
-  return <div>Learn Page</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: 'English', imageSrc: '/uk.svg' }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="English" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
