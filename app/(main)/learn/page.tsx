55bcd3150deabd453dbdff15db9f6f071b65b8a6
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-23 21:06:57 +0700

    Add courses selection page UI

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,18 @@ import { StickyWrapper } from '@/components/sticky-wrapper';
 import { FeedWrapper } from '@/components/feed-wrapper';
 import { Header } from './_components/header';
 import { UserProgress } from '@/components/user-progress';
+import { getUserProgress } from '@/db/queries';
+import { redirect } from 'next/navigation';
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses');
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
