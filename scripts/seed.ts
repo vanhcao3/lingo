6e8f1b4ae84532735867b7f4e03ec1d9563b26e9
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-23 23:01:22 +0700

    Add data model

---
+++ scripts/seed.ts
@@ -0,0 +1,113 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+import * as schema from '@/db/schema';
+
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log('Seeding the database');
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: 'English',
+        imageSrc: '/uk.svg',
+      },
+      {
+        id: 2,
+        title: 'Italian',
+        imageSrc: '/it.svg',
+      },
+      {
+        id: 3,
+        title: 'French',
+        imageSrc: '/Fr.svg',
+      },
+      {
+        id: 4,
+        title: 'Croatian',
+        imageSrc: '/hr.svg',
+      },
+      {
+        id: 5,
+        title: 'Spanish',
+        imageSrc: '/es.svg',
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 5,
+        title: 'Unit 1',
+        description: 'Spanish basics',
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: 'Nouns',
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: 'SELECT',
+        order: 1,
+        question: 'Choose the word that is equivalent to "the man":',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        imageSrc: '/man.svg',
+        correct: true,
+        text: 'el hombre',
+        audioSrc: '/es_man.mp3',
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        imageSrc: '/woman.svg',
+        correct: false,
+        text: 'la mujhe',
+        audioSrc: '/es_woman.mp3',
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        imageSrc: '/robot.svg',
+        correct: false,
+        text: 'el robot',
+        audioSrc: '/es_robot.mp3',
+      },
+    ]);
+
+    console.log('Seeding finished');
+  } catch (error) {
+    console.error(error);
+    throw new Error('Failed to seed the database=');
+  }
+};
+
+main();
