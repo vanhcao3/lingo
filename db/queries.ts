c830c25c3f0162a5987f8bde6cfda1f669af53ef
Cao Viet Anh <caovietanhbk@gmail.com>
2024-05-23 23:01:49 +0700

    Add getUnits with progress action

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,13 @@ import { cache } from 'react';
 import db from './drizzle';
 import { eq } from 'drizzle-orm';
 import { auth } from '@clerk/nextjs';
-import { courses, userProgress } from './schema';
+import {
+  courses,
+  userProgress,
+  units,
+  lessons,
+  challengeProgress,
+} from './schema';
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -18,6 +24,48 @@ export const getUserProgress = cache(async () => {
   });
 
   return data;
+});
+
+export const getUnits = cache(async () => {
+  const { userId } = await auth();
+  const userProgress = await getUserProgress();
+
+  if (!userId || !userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+  return normalizedData;
 });
 
 export const getCourses = cache(async () => {
